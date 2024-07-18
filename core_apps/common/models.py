99fc4827d3b2b44ece25dc5d9b4f0d4842652d13
Ekuesky <ayiekue9127@gmail.com>
2024-07-18 14:16:39 +0000

    add common app

---
+++ core_apps/common/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import uuid
+
+
+class TimeStampedModel(models.Model):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True # Tell django this class must not be stored in database
+        ordering = ["-created_at","-updated_at"]
+
