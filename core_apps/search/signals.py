d452522b25e995aac28fa505aa716974c6799e2b
Ekuesky <ayiekue9127@gmail.com>
2024-09-23 20:07:13 +0000

    feat:created search module for elasticsearch, documents,serializers and signals

---
+++ core_apps/search/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import  post_save, post_delete
+from django.dispatch import receiver
+
+from django_elasticsearch_dsl.registries import registry
+from core_apps.articles.models import Article
+
+
+@receiver(post_save, sender=Article)
+def update_document(sender, instance, created=False,  **kwargs):
+    registry.update(instance)
+
+
+
+@receiver(post_delete, sender=Article)
+def delete_document(sender, instance, **kwargs):
+    registry.delete(instance)
+
+
